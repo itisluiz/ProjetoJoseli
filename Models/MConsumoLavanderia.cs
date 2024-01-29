d76d11ebc70199aba4463ff366b55535157786d2
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2024-01-28 21:51:33 -0300

    CRUD Consumo

--- Models/MConsumoLavanderia.cs
+++ Models/MConsumoLavanderia.cs
@@ -8,9 +8,18 @@ public class MConsumoLavanderia
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Codigo { get; set; }
 
-    public DateTime Data { get; set; }
+    public DateTime? Data { get; set; }
 
     public MEstadia? Estadia { get; set; }
 
     public MServicoLavanderia? ServicoLavanderia { get; set; }
+
+    private MConsumoLavanderia() { }
+
+    public MConsumoLavanderia(MEstadia estadia, MServicoLavanderia servicoLavanderia)
+    {
+        this.Data = DateTime.Now;
+        this.Estadia = estadia;
+        this.ServicoLavanderia = servicoLavanderia;
+    }
 }
--- Models/MConsumoRefeicao.cs
+++ Models/MConsumoRefeicao.cs
@@ -8,15 +8,26 @@ public class MConsumoRefeicao
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Codigo { get; set; }
 
-    public bool EntregaQuarto { get; set; }
+    public bool? EntregaQuarto { get; set; }
 
     [Column(TypeName = "decimal(12,2)")]
-    public decimal Custo { get; set; }
+    public decimal? Custo { get; set; }
 
     [MaxLength(64)]
     public string? Descricao { get; set; }
 
-    public DateTime Data { get; set; }
+    public DateTime? Data { get; set; }
 
     public MEstadia? Estadia { get; set; }
+
+    private MConsumoRefeicao() { }
+
+    public MConsumoRefeicao(bool entregaQuarto, decimal custo, string descricao, MEstadia estadia)
+    {
+        this.EntregaQuarto = entregaQuarto;
+        this.Custo = custo;
+        this.Descricao = descricao;
+        this.Data = DateTime.Now;
+        this.Estadia = estadia;
+    }
 }
